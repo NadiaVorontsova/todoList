e163d82e410be97512a87a6c27e5bf6a576ef9f6
NadiaVorontsova <Vorontsovvaaa@gmail.com>
2022-10-13 03:06:03 +0300

    add date

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const inputTask = document.querySelector(".input_task");
 const addButton = document.querySelector(".add__button");
 const listOfTasks = document.querySelector(".todo__list");
+const dayOfTheWeek = document.querySelector(".day");
+const fullDate = document.querySelector(".date");
 
 let todoList = [];
+
+if (localStorage.getItem("todo")) {
+  todoList = JSON.parse(localStorage.getItem("todo"));
+  displayItemsOfList();
+}
+
+function getDayOfTheWeek() {
+  let now = new Date();
+  let date =
+    now.getDate() + "/" + (now.getMonth() + 1) + "/" + now.getFullYear();
+  let options = { weekday: "long" };
+  dayOfTheWeek.innerHTML = new Intl.DateTimeFormat("en-US", options).format(
+    now
+  );
+  fullDate.innerHTML = date;
+}
+getDayOfTheWeek();
 
 addButton.addEventListener("click", () => {
   let newTask = {
@@ -10,38 +29,67 @@ addButton.addEventListener("click", () => {
     checked: false,
     important: false,
   };
-
   todoList.push(newTask);
 
   displayItemsOfList();
+  localStorage.setItem("todo", JSON.stringify(todoList));
 });
-
-const displayItemsOfList = () => {
+//localStorage.clear();
+function displayItemsOfList() {
+  resetInput();
   let task = "";
   todoList.forEach(
     (item, i) =>
       (task += `
-        <li class="todo__list__item">
-            <label class="item" for="item_${i}">&#9998; ${item.todo}</label>
-            <div class="menu_for_item">
-                <input type="checkbox" id="item_${i}" class="task_check" />
-                <img src="assets/image/star.png" width="15" height="15" alt="star" />
-                <img src="assets/image/bin.png" width="15" height="15" alt="bin"/>
-            </div>
+        <li class="todo__list__item" data-value=${item.todo}>
+          <div>
+            <span> ${i + 1}. </span>
+            <label class="item" for="item_${i}">${item.todo}</label>
+          </div>
+          <div class="menu_for_item">
+            <input type="checkbox" id="item_${i}" class="task_check" ${
+        item.checked ? "checked" : ""
+      }/>
+            <img src="assets/image/star.png" width="15" height="15" alt="star" data-important=${
+              item.todo
+            } />
+            <img src="assets/image/bin.png" width="15" height="15" alt="bin" data-delete=${
+              item.todo
+            } />
+          </div>
         </li>`)
   );
   listOfTasks.innerHTML = task;
-};
+}
 
-// listOfTasks.addEventListener("click", ({ target }) => {
-//   let index = -1;
-//   if (target.alt === "bin") {
-//     todoList.forEach((item) => {
-//       console.log(item);
-//       index = todoList.indexOf(item);
-//       console.log(index);
-//     });
-//     //todoList.splice(index, 1);
-//   }
-//   displayItemsOfList();
-// });
+function resetInput() {
+  inputTask.value = "";
+}
+
+listOfTasks.addEventListener("change", ({ target }) => {
+  let forLabel = listOfTasks.querySelector(
+    "[for=" + target.getAttribute("id") + "]"
+  ).innerHTML;
+  todoList.forEach((item) => {
+    if (item.todo === forLabel) {
+      item.checked = !item.checked;
+      localStorage.setItem("todo", JSON.stringify(todoList));
+    }
+  });
+});
+
+listOfTasks.addEventListener("click", ({ target }) => {
+  todoList.forEach((item) => {
+    if (item.todo === target.dataset.delete) {
+      let index = todoList.indexOf(target.dataset.delete);
+      console.log(item.todo, target.dataset.delete, index);
+      //todoList.splice(index, 1);
+    }
+    if (item.todo === target.dataset.important) {
+      item.important = !item.important;
+      let data = listOfTasks.querySelector("[data-value=" + item.todo + "]");
+      data.classList.toggle("important_task");
+      localStorage.setItem("todo", JSON.stringify(todoList));
+    }
+  });
+});
